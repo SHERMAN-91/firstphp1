2bc6fa284e75c812f03bc18650ea19fc2726a8dd
SHERMAN-91 <93903940+SHERMAN-91@users.noreply.github.com>
2022-11-13 02:19:08 +0200

    Update conn.php

--- newEtoima/conn.php
+++ newEtoima/conn.php
@@ -1,13 +1,14 @@
 <?php
 
-$sdn='mysql: host=localhost; dbname=etioma';
-$user="root";
-$pass="";
+$sdn='mysql: host=sql7.freemysqlhosting.net; dbname=sql7559479';
+$user="sql7559479";
+$pass="VLcnpAlQ3D";
+$portD="3306";
 $options=array(
     PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'
 );
 try{
-$db= new PDO($sdn, $user, $pass, $options);
+$db= new PDO($sdn, $user, $pass,$portD, $options);
 $db -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 
